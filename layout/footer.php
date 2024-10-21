7c97ecbe83d0cf4c2a105984b2a6334142687efd
dawillygene <eliamariki2000@gmail.com>
2024-10-21 03:17:32 -0400

    add the header and footer

---
+++ layout/footer.php
@@ -0,0 +1,92 @@
+
+<footer class="footer">
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-3 col-xl-4 footer__widget footer__widget-about">
+              <div class="footer__widget-content">
+                <img src="assets/images/logo/logo-footer.png" alt="logo" class="mb-30">
+                <p class="mb-20">A leading developer of A-grade commercial, industrial and residential projects in USA.
+                  Since its foundation the company has doubled its turnover year on year with its staff.</p>
+                <ul class="social__icons list-unstyled">
+                  <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul> 
+              </div>
+            </div>
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 footer__widget footer__widget-nav">
+              <h6 class="footer__widget-title">Services</h6>
+              <div class="footer__widget-content">
+                <nav>
+                  <ul class="list-unstyled">
+                    <li><a href="#">Construction Manage</a></li>
+                    <li><a href="#">Construction Consultants</a></li>
+                    <li><a href="#">Architecture & Building</a></li>
+                    <li><a href="#">Home Renovations</a></li>
+                    <li><a href="#">Tiling & Painiting</a></li>
+                    <li><a href="#">Interior Design</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 footer__widget footer__widget-nav">
+              <h6 class="footer__widget-title">Company</h6>
+              <div class="footer__widget-content">
+                <nav>
+                  <ul class="list-unstyled">
+                    <li><a href="about-us.html">About Us</a></li>
+                    <li><a href="leadership-team.html">Meet Our Team</a></li>
+                    <li><a href="blog.html">News & Media</a></li>
+                    <li><a href="projects-grid.html">Case Studies</a></li>
+                    <li><a href="contacs.html">Contacts</a></li>
+                    <li><a href="careers.html">Careers</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 footer__widget footer__widget-newsletter">
+              <h6 class="footer__widget-title">Newsletter</h6>
+              <div class="footer__widget-content">
+                <p>Sign up for industry alerts, our latest news, thoughts, and insights from Promina.</p>
+                <form class="widget__newsletter-form">
+                  <div class="form-group mb-0">
+                    <input type="text" class="form-control" placeholder="Your Email Address">
+                    <button type="submit" class="btn btn__primary">
+                      <i class="icon-arrow-right"></i>
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <p class="text-right footer__more-info mt-20 mb-0">Have a question? <a href="#">Click here</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-sm-12 col-md-12 col-lg-12 d-flex flex-wrap justify-content-between">
+              <nav>
+                <ul class="footer__copyright-links list-unstyled d-flex flex-wrap mb-0">
+                  <li><a href="#">Terms & Conditions </a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                  <li><a href="#">Sitemap</a></li>
+                </ul>
+              </nav>
+              <p class="mb-0 color-white"> &copy; 2020 Promina. With Love by
+                <a href="http://themeforest.net/user/7oroof">7oroof.com</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <button id="scrollTopBtn"><i class="fa fa-long-arrow-up"></i></button>
+
+  </div>
+  <script src="assets/js/jquery-3.5.1.min.js"></script>
+  <script src="assets/js/plugins.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
